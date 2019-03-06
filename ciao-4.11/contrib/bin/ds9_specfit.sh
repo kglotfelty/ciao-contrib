bfc0e21af05f5edeefca3ef30a6e5f2ddc40cc68
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2019-03-06 11:13:52 -0500

    update sherpa specfit to use blt for plotting

--- ciao-4.11/contrib/bin/ds9_specfit.sh
+++ ciao-4.11/contrib/bin/ds9_specfit.sh
@@ -226,16 +226,76 @@ except:
 print( "\nPhoton Flux = %s photon/cm^2/s\n" % sherpa.calc_photon_flux())
 print( "Energy Flux = %s ergs/cm^2/s\n" % sherpa.calc_energy_flux())
 
-
-try:
-  pychips.disconnect()
-  pychips.connect("${ds9}")
-  sherpa.plot_fit_delchi()
-except:
-  pass
-
-
 sherpa.save("$sav", clobber=True)
+
+
+# -- Emulate the 'plot_fit()' sherpa command, 
+
+import subprocess
+
+def xpa_plot_cmd( access_point, command ):
+    """Wrapper around xpaset for plot commands"""
+    
+    cc = ["xpaset", "-p", access_point, "plot" ]
+    cc.extend( command.split(' '))    
+    xpa = subprocess.Popen(cc)
+    xpa.communicate()
+
+
+def plot_data(access_point,xx, ex, yy, ey, title, x_label, y_label):
+    """Plot the data"""
+    
+    cmd = ["xpaset", access_point, "plot"]    
+    cmd.extend( ["new", "line", 
+        "{{{0}}}".format(title), 
+        "{{{0}}}".format(x_label), 
+        "{{{0}}}".format(y_label),
+        "xyey"
+        ] )
+
+    # Plot the data
+    xpa = subprocess.Popen( cmd, stdin=subprocess.PIPE ) 
+    for vv in zip(xx, yy, ey):
+        pair = " ".join( [str(x) for x in vv])+"\n"        
+        pb = pair.encode()
+        xpa.stdin.write(pb)        
+    xpa.communicate()
+
+    # Make pretty
+    xpa_plot_cmd(access_point, "shape circle")
+    xpa_plot_cmd(access_point, "shape fill yes")
+    xpa_plot_cmd(access_point, "shape color cornflowerblue")
+    xpa_plot_cmd(access_point, "error color cornflowerblue")
+    #xpa_plot_cmd(access_point, "error cap yes")
+    xpa_plot_cmd(access_point, "color white")
+    
+
+def plot_model(access_point,x_vals, y_vals):
+    """Plot the model"""
+    
+    cmd = ["xpaset", access_point, "plot"]
+    cmd.extend( ["data", "xy"] )
+
+    xpa = subprocess.Popen( cmd, stdin=subprocess.PIPE ) 
+    for x,y in zip(x_vals, y_vals):
+        pair = "{} {}\n".format(x,y)
+        pb = pair.encode()
+        xpa.stdin.write(pb)        
+    xpa.communicate()
+    #xpa_plot_cmd(access_point, "shape none")
+    xpa_plot_cmd(access_point, "shape fill no")
+    xpa_plot_cmd(access_point, "color orange")
+    xpa_plot_cmd(access_point, "shape color orange")
+    xpa_plot_cmd(access_point, "width 4")
+
+d = sherpa.get_data_plot()
+m = sherpa.get_model_plot()
+x = (m.xhi+m.xlo)/2.0
+
+plot_data( "${ds9}", d.x, d.xerr/2.0, d.y, d.yerr, "${spi}", "Energy [keV]", "Count Rate [c/s]")
+
+plot_model( "${ds9}", x, m.y)
+
 
 EOF
 
